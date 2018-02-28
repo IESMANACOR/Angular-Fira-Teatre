553f43e0cb6ee8a1a71c81c1b148a5bc76208880
Mateu <mateu_21_5_96@hotmail.com>
2018-02-28 22:22:51 +0100

    funcionalitats afegides

---
+++ src/app/quisom/quisom.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Http} from '@angular/http'
+import { promise } from 'selenium-webdriver';
+
+@Component({
+  selector: 'app-quisom',
+  templateUrl: './quisom.component.html',
+  styleUrls: ['./quisom.component.css']
+})
+export class QuisomComponent implements OnInit {
+
+  sala:any;
+  constructor(private http: Http) {
+    this.http.get('http://www.f.dawman.info/rest/public/sala/1')
+    .toPromise()
+    .then(respuesta=> this.sala = respuesta.json());
+   }
+
+  ngOnInit() {
+  }
+
+}
