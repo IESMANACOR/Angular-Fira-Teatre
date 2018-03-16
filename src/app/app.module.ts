8a27e3f3531859da3e37c548f96cfca5690526b1
jordiriera <jrierarayo@gmail.com>
2018-03-16 01:38:51 +0100

    arreglant app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,7 +45,8 @@ import { EntradasComponent } from './entradas/entradas.component';
 
 import { PerfilComponent } from './perfil/perfil.component';
 import { UsuariComponent } from './usuari/usuari.component'
-import { NgxQRCodeModule } from 'ngx-qrcode3';
+
+
 const routes: Routes = [
 
 ];
@@ -72,10 +73,10 @@ const routes: Routes = [
     QuisomComponent,
     RegistreUsuariComponent,
     ComprasUsuariComponent,
-   
+
     EntradasComponent,
     PerfilComponent,
-   
+
     UsuariComponent
   ],
   imports: [
@@ -93,13 +94,13 @@ const routes: Routes = [
 
   ],
   providers: [DetallService,
-    EspectacleService, 
+    EspectacleService,
     RegistreService,
     ComprarService,
     CarretService,
     RegistreUsuariService,
     FormulariService],
-    
+
   bootstrap: [AppComponent],
 
 })
